fa194ed227a615b8e939677a34189297a6ef8685
Ajay <Ajay4uu.ak@gmail.com>
2023-03-20 16:31:49 +0530

    learning react with api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const App =()=>{
 
-export default App;
+    const [title,setTitle] = useState('')
+    const [body,setBody]=useState("")
+    const [author,setAuthor] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        const  blog = {title,body,author}
+
+        console.log(blog)
+
+    }
+
+    return(
+        <div>
+            <h1>Blog title</h1>
+            <form onSubmit={handleSubmit}>
+            <h3>Add a new Blog</h3>
+            <input type="text" required value={title} 
+            onChange={(e)=>{
+                setTitle(e.target.value)
+            }}/>
+            <h3>Blog Ttitle</h3>
+            <textarea required
+            value={body}
+            onChange={(e)=>{setBody(e.target.value)}}></textarea>
+            <h3>Blog Author</h3>
+            <input type="text"
+             required
+             onChange={(e)=>{setAuthor(e.target.value)}}/>
+              <div>
+                <button>Add Blog</button>
+              </div>
+            </form>
+               
+            <h1>Title: {title}</h1>
+            <h1>Body :{body}</h1>
+            <h1>Author: {author}</h1>
+       
+        </div>
+    )
+ }
+
+
+ export default App
